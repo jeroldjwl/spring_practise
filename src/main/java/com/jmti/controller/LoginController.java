c8039573a256f6a2549ec98ac9477d498551c194
jeroldjwl <1017794273@qq.com>
2016-04-22 19:03:49 +0800

    Commit test

---
+++ src/main/java/com/jmti/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.jmti.controller;
+
+/**
+ * Created by jerold on 2016/4/22.
+ */
+public class LoginController {
+    public LoginController() {
+
+    }
+
+    public void test1() {
+
+    }
+}
